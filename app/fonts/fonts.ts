80abd647f00a637baee646e89e7518d3dce489f6
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-05-19 21:53:24 +0300

    adding fonts, fixing layout

---
+++ app/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import { Great_Vibes} from "next/font/google";
+
+export const greatVibes = Great_Vibes({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
+
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/atoms/NavBar";
+import Banner from "@/components/sections/Banner";
+
+export default function Home() {
+  return (
+    <div className="bg-black p-2">
+      <NavBar />
+      <Banner />
+    </div>
+  );
+}
