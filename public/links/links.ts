e5195d79860158c2e587158f171823ce7d6a4fbe
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-05-19 21:50:59 +0300

    adding images and links

---
+++ public/links/links.ts
@@ -0,0 +1,9 @@
+export const links = [
+    { href: "/", label: "Home" },
+    { href: "/menu", label: "Menu" },
+    { href: "/about", label: "About" },
+    { href: "/contact", label: "Best Dishes" },
+    { href: "/reservation", label: "Our Chefs" },
+    { href: "/gallery", label: "Reviews" },
+    { href: "/reviews", label: "Contact Us" },
+]
